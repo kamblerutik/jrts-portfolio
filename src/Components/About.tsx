44756cdfe0d89c563de2d4041f206b17a14a3c6e
jr_ts_31 <kamblerutik155@gmail.com>
2025-03-18 18:48:45 +0530

    push

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <Box
       id="about"
       sx={{
-        height: { xs: "fit", md: "100vh" },
+        height: { xs: "fit", md: "fit" },
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
@@ -14,7 +14,7 @@ const About = () => {
     >
       <Typography
         sx={{
-          fontSize: { xs: "1rem", md: "1.5rem" },
+          fontSize: { xs: "1rem", md: "1.3rem" },
         }}
       >
         I'm a developer passionate about creating accessible, pixel-perfect user
